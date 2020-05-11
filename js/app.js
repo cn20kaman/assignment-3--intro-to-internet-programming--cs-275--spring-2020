f18fc0e17105f8baf503aeaa18e87525347b273c
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-05-11 12:30:19 -0400

    for each cell test whether we need to rotate or rotateBack

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ window.onload = ( ) => {
     let cells = document.getElementsByTagName(`td`);
 
     const rotate = function(e){
+        for(let cell of cells){
+            if(cell.className === `rotate`){
+                cell.className=`rotateBack`;
+            }
+            else if(cell===e.target && e.target.className !== `rotate`){
+                e.target.className = `rotate`;
+            }
+        }
     };
 
     for(let cell of cells){
