3bbeae09f6fd9bdfb5389c9d2235719532e9ed74
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-05-11 12:29:42 -0400

    create rotate function with param MouseEvent e

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 window.onload = ( ) => {
     let cells = document.getElementsByTagName(`td`);
 
+    const rotate = function(e){
+    };
+
     for(let cell of cells){
         cell.addEventListener(`click`,rotate);
     }
